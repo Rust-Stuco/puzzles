183cfd00ce3ef5bc82f8d4cd727187634eb756d0
Jack Duvall <10897431+duvallj@users.noreply.github.com>
2022-03-23 18:21:05 -0400

    Fix 08 test.sh so autograder is happy

--- 08_error_handling/test.sh
+++ 08_error_handling/test.sh
@@ -15,7 +15,7 @@ echo -ne "10\nhello\n" | cargo run
 echo -ne "foobar\n" | cargo run
 
 # Test a few cases
-[[ $(echo -ne "69\n1337\n" | cargo run) -eq 7257550166497394936 ]]
-[[ $(echo -ne "420\n15251\n" | cargo run) -eq 11604359411781646366 ]]
-[[ $(echo -ne "98008\n98013\n" | cargo run) -eq 12880269392473839412 ]]
-[[ $(echo -ne "9223372036854775908\n18446744073709427184\n" | cargo run) -eq 9144881374108674624 ]]
+if (( $(echo -ne "69\n1337\n" | cargo run) != 7257550166497394936 )) ; then exit 1 ; fi
+if (( $(echo -ne "420\n15251\n" | cargo run) != 11604359411781646366 )) ; then exit 1 ; fi
+if (( $(echo -ne "98008\n98013\n" | cargo run) != 12880269392473839412 )) ; then exit 1 ; fi
+if (( $(echo -ne "9223372036854775908\n18446744073709427184\n" | cargo run) != 9144881374108674624 )) ; then exit 1 ; fi
